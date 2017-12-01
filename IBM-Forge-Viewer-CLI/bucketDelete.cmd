fc3a44d48971bfcba1358a72158c60b74c69c216
Doug Wood <doug.wood@us.ibm.com>
2017-12-01 17:33:52 -0500

    Update for changes in REST proxy and proxy jar name

--- IBM-Forge-Viewer-CLI/bucketDelete.cmd
+++ IBM-Forge-Viewer-CLI/bucketDelete.cmd
@@ -1,2 +1,2 @@
 echo off
-java -cp IBM-Forge-CLI.jar;commons-codec-1.9.jar;javax.json-1.0.2.jar;json4j.jar psdi.app.bim.viewer.dataapi.cli.BucketDelete %1 %2 
+java -cp IBM-Forge-1.0.jar;commons-codec-1.9.jar;javax.json-1.0.2.jar;json4j.jar psdi.app.bim.viewer.dataapi.cli.BucketDelete %1 %2 
